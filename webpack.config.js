0e902013a200de12c144d145837c066ab73158c7
Mik <mahmoudothman.pro@gmail.com>
2022-04-28 14:32:07 +0700

    fix webpack react external

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
 var path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve("build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }
-    ]
-  },
-  externals: {
-    react: "react"
-  }
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("build"),
+        filename: "index.js",
+        libraryTarget: "commonjs2"
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    },
+    externals: {
+        "react": {
+            "commonjs": "react",
+            "commonjs2": "react",
+            "amd": "react",
+            "root": "React"
+        },
+        "react-dom": {
+            "commonjs": "react-dom",
+            "commonjs2": "react-dom",
+            "amd": "react-dom",
+            "root": "ReactDOM"
+        }
+    }
 };
